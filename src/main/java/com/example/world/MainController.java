3a04672902d983b95122e522559a087930d2dc6d
tjqls <tjqls2013@gmail.com>
2023-07-17 18:15:54 +0900

    dg

--- src/main/java/com/example/world/MainController.java
+++ src/main/java/com/example/world/MainController.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+@Controller
+public class MainController {
 
-public class MainController {}
+    @GetMapping("/hi")
+    public String hi(){
+        return "navbar";
+    }
+}
 
