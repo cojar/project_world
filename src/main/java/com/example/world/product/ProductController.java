769ae5c3bef6747efd2622e527a2240d32e8d76c
ohgil <ohgilhwan1957@gmail.com>
2023-07-19 10:46:11 +0900

    리스트페이지 1차 작업

--- src/main/java/com/example/world/product/ProductController.java
+++ src/main/java/com/example/world/product/ProductController.java
@@ -1,4 +1,17 @@
 package com.example.world.product;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/product/list")
+    public String list(){
+        return "product_list";
+    }
 }
--- src/main/java/com/example/world/product/ProductService.java
+++ src/main/java/com/example/world/product/ProductService.java
@@ -1,4 +1,11 @@
 package com.example.world.product;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
 }
