4504dc3f1f42050ac18cecb7481435e1d514236f
tjqls <tjqls2013@gmail.com>
2023-07-21 10:04:10 +0900

    리스트페이지 각테마별 정렬

--- src/main/java/com/example/world/product/ProductController.java
+++ src/main/java/com/example/world/product/ProductController.java
@@ -1,17 +1,68 @@
 package com.example.world.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/product")
 public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/product/list")
-    public String list(){
+    @GetMapping("/list/All")
+    public String allList(Model model, @RequestParam(value = "page",defaultValue = "0")int page){
+        Page<Product> paging = this.productService.AllTheme(page);
+        model.addAttribute("paging",paging);
         return "product_list";
     }
+
+
+    @GetMapping(value = "/list/{thema}")
+    public String productList(Model model, @PathVariable("thema") String key, @RequestParam(value = "page",defaultValue = "0")int page){
+        String themekey;
+        if(key.equals("rpg")){
+            themekey = "RPG";
+            Page<Product> paging = this.productService.getTheme(page, themekey);
+            model.addAttribute("paging",paging);
+        }else if(key.equals("fps")){
+            themekey="FPS";
+            Page<Product> paging = this.productService.getTheme(page, themekey);
+            model.addAttribute("paging",paging);
+        }else if(key.equals("thriller")){
+            themekey="Thriller";
+            Page<Product> paging = this.productService.getTheme(page, themekey);
+            model.addAttribute("paging",paging);
+        }else if(key.equals("arcade")){
+            themekey="Arcade";
+            Page<Product> paging = this.productService.getTheme(page, themekey);
+            model.addAttribute("paging", paging);
+        }else if(key.equals("sports")){
+            themekey="Sports";
+            Page<Product> paging = this.productService.getTheme(page, themekey);
+            model.addAttribute("paging",paging);
+        }else if(key.equals("simulation")){
+            themekey="Simulation";
+            Page<Product> paging = this.productService.getTheme(page, themekey);
+            model.addAttribute("paging",paging);
+        }else if(key.equals("etc")){
+            themekey="ETC";
+            Page<Product> paging = this.productService.getTheme(page, themekey);
+            model.addAttribute("paging",paging);
+        }
+
+        return "product_list";
+    }
+
+
+// all priduct/list/
+
 }
--- src/main/java/com/example/world/product/ProductRepository.java
+++ src/main/java/com/example/world/product/ProductRepository.java
@@ -1,6 +1,15 @@
 package com.example.world.product;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+ Page<Product> findAll(Specification<Product> spec, Pageable pageable );
+ Page<Product> findAll(Pageable pageable);
 }
