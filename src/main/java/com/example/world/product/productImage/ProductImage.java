7fa7661d4445d128c442f40dd6535c169d906413
ohgil <ohgilhwan1957@gmail.com>
2023-08-09 13:31:00 +0900

    병합

--- src/main/java/com/example/world/product/productImage/ProductImage.java
+++ src/main/java/com/example/world/product/productImage/ProductImage.java
@@ -1,25 +1,25 @@
-package com.example.world.product.productImage;
-
-import com.example.world.file.UploadedFile;
-import com.example.world.product.Product;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
-@Entity
-public class ProductImage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(length = 50)
-    private String name;
-
-    @OneToOne
-    private UploadedFile image;
-
-    @ManyToOne
-    private Product product;
-}
+//package com.example.world.product.productImage;
+//
+//import com.example.world.file.UploadedFile;
+//import com.example.world.product.Product;
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Setter
+//@Getter
+//@Entity
+//public class ProductImage {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(length = 50)
+//    private String name;
+//
+//    @OneToOne
+//    private UploadedFile image;
+//
+//    @ManyToOne
+//    private Product product;
+//}
